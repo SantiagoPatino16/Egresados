0a2ce00da34a87985ea6bb880e8302abb5c6fcdf
SantiagoPatiño16 <sapara.016@gmail.com>
2025-11-16 11:48:57 -0500

    Mensajería update

--- DataAccess/ConnectionDB/RSContext.cs
+++ DataAccess/ConnectionDB/RSContext.cs
@@ -23,6 +23,11 @@ namespace DataAccess.ConnectionDB
         public DbSet<AttributesCategoriesJobs> CategoriasEmpleo { get; set; }
         public DbSet<AttributesWorkModalities> ModalidadesTrabajo { get; set; }
         public DbSet<AttributesStatusApplication> EstadoPostulacion { get; set; }
+        public DbSet<AttributesMessages> Mensajes { get; set; }
+        public DbSet<AttributesMessageReactions> Chats { get; set; }
+        public DbSet<AttributesMessageAttachments> Adjuntos { get; set; }
+
+
 
         //Permite las relaciones y añade el "DELETE ON CASCADE" de Publicaciones --> comentarios
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
@@ -43,6 +48,12 @@ namespace DataAccess.ConnectionDB
                 .WithMany()
                 .HasForeignKey(c => c.IdPublicacion)
                 .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<AttributesMessages>()
+                .HasRequired(m => m.Receptor)
+                .WithMany()
+                .HasForeignKey(m => m.IdReceptor)
+                .WillCascadeOnDelete(false);
         }
 
     }
